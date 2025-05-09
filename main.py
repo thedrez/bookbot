773d1f428c2f38ddaa5b2f1dda33cdadffe29845
thedrez <thedrez@icloud.com>
2025-05-09 12:15:14 -0600

    Ch3-L1 Create a Report

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-from stats import get_num_words,get_char_counts
-
+from stats import create_report
 
 def get_book_text(path_to_book):
-
     with open(path_to_book, 'r') as book:
         contents_of_book = book.read()
-
     return contents_of_book
 
-
 def main():
-    text_of_book = get_book_text("books/frankenstein.txt")
-    get_num_words(text_of_book)
-    testone = "testone"
-    #print(len(testone))
-    #for character in range(0,len(testone)):
-    #    print(testone[character])
-    get_char_counts(text_of_book)
+    relative_path_to_book = "books/frankenstein.txt"
+    create_report(relative_path_to_book,get_book_text(relative_path_to_book))
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,19 +1,45 @@
+def sort_on(dict):
+    return dict["num"]
+
 def get_num_words(text):
     number_of_words = text.split()
-    print(len(number_of_words), "words found in the document")
+    return len(number_of_words)
 
-def get_char_counts(text):
-    char_counts = {}
+def get_char_counts_dict(text):
+    char_counts_dict = {}
     lowercase_text = text.lower()
     for character in range(0,len(lowercase_text)):
         my_char = lowercase_text[character]
         #print(my_char)
-        if my_char in list(char_counts):
-            #print("found: ",my_char," with value:",char_counts[my_char])
-            char_counts.update({my_char: char_counts[my_char] + 1})
+        if my_char in list(char_counts_dict):
+            #print("found: ",my_char," with value:",char_counts_dict[my_char])
+            char_counts_dict.update({my_char: char_counts_dict[my_char] + 1})
         else:
             #print("must add: ",my_char)
-            char_counts.update({my_char: 1})
+            char_counts_dict.update({my_char: 1})
 
-    print(char_counts)
+    #print(char_counts_dict)
+    return char_counts_dict
+
+def get_sorted_char_count_array(unsorted_char_count_dict):
+    sorted_char_count_array = []
+    for key,value in unsorted_char_count_dict.items():
+        if key.isalpha():
+            sorted_char_count_array.append({"char": key, "num": value})
+    
+    #print(type(sort_on(sorted_char_count_array[0])))
+
+    sorted_char_count_array.sort(reverse=True, key=sort_on)
+    #print(sorted_char_count_array)
+    return sorted_char_count_array
+
+def create_report(relative_path_to_book,book_text):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at",relative_path_to_book,"...")
+    print("----------- Word Count ----------")
+    print("Found",get_num_words(book_text),"total words")
+    print("--------- Character Count -------")
+    for item in get_sorted_char_count_array(get_char_counts_dict(book_text)):
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
