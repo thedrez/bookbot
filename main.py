dba82fe164d215a8e77d53954ef3868a622015dd
thedrez <thedrez@icloud.com>
2025-05-12 11:56:43 -0600

    Ch3-L2 added file argument processing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import os
 from stats import create_report
 
 def get_book_text(path_to_book):
@@ -6,7 +8,18 @@ def get_book_text(path_to_book):
     return contents_of_book
 
 def main():
-    relative_path_to_book = "books/frankenstein.txt"
+    print(sys.argv[1:])
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        relative_path_to_book = sys.argv[1]
+
+    if not os.path.exists(relative_path_to_book):
+        print(f"File {relative_path_to_book} does not exist!")
+        sys.exit(1)
+
+    print(f"Found {relative_path_to_book} in filesystem.")
     create_report(relative_path_to_book,get_book_text(relative_path_to_book))
 
 if __name__ == "__main__":
